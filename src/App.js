e4ccb1fb226f3a43c9c74a439a4027af2b525754
Nivas Reddy <srinivasreddysagi@gmail.com>
2022-06-21 00:48:08 +0530

    Almost there! A bug needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,133 @@
-import './App.css';
-import Board from './Board';
+import { useState } from "react";
+import "./App.css";
+import Board from "./Board";
 
 function App() {
-  return (
-    <div>
-      <Board />
-    </div>
-  );
+    const [data, setData] = useState({
+        history: [
+            {
+                squares: Array(9).fill(null),
+            },
+            // {
+            //     squares: ["X", null, null, null, null, null, null, null, null],
+            // },
+        ],
+        xIsNext: true,
+        stepCount: 0,
+    });
+
+    // let state = {
+    //     history: [
+    //         {
+    //             squares: Array(9).fill(null),
+    //         },
+    //         {
+    //             squares: Array(9).fill("O"),
+    //         },
+    //     ],
+    //     xIsNext: true,
+    //     stepCount: 0,
+    // };
+    const { history } = data;
+    const winner = calculateWinner(data.history[data.stepCount].squares);
+    // console.log(data.history);
+    const moves = history.map((obj, index) => {
+        // console.log(index);
+        return (
+            <li key={index}>
+                <button onClick={goTo(index)}>
+                    {index > 0 ? "Move#" + index : "Reset"}
+                </button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (data.xIsNext ? "X" : "O");
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (
+                squares[a] &&
+                squares[a] === squares[b] &&
+                squares[a] === squares[c]
+            ) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    function handleClick(i) {
+        const history = data.history.slice(0, data.stepCount + 1);
+        const squares = history[history.length - 1].squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = data.xIsNext ? "X" : "O";
+        setData({
+            history: history.concat([
+                {
+                    squares: squares,
+                },
+            ]),
+            stepCount: history.length,
+            xIsNext: !data.xIsNext,
+        });
+        console.log(data.history);
+        // state = {
+        //     history: history.concat([
+        //         {
+        //             squares: squares,
+        //         },
+        //     ]),
+        //     stepCount: history.length,
+        //     xIsNext: !state.xIsNext,
+        // };
+    }
+
+    function goTo(index) {
+        setData({
+            xIsNext: index % 2 === 0,
+            stepCount: index,
+        });
+        // state = {
+        //     xIsNext: index % 2 === 0,
+        //     stepCount: index,
+        // };
+    }
+
+    return (
+        <div className="game-main">
+            <div className="game">
+                <Board
+                    squares={data.history[data.stepCount].squares}
+                    onClick={(i) => {
+                        handleClick(i);
+                    }}
+                />
+            </div>
+            <div className="travel">
+                <div>{status}</div>
+                <ol className="btn-container">{moves}</ol>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -1,29 +1,36 @@
-function Board() {
+function Square(props) {
+    return <button onClick={props.onClick}>{props.value}</button>;
+}
+
+export default function Board(props) {
+    function renderSquare(i) {
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => {
+                    props.onClick(i);
+                }}
+            />
+        );
+    }
+
     return (
         <div>
-            <div>
-                <Square />
-                <Square />
-                <Square />
+            <div className="row-1">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
-            <div>
-                <Square />
-                <Square />
-                <Square />
+            <div className="row-2">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
-            <div>
-                <Square />
-                <Square />
-                <Square />
+            <div className="row-3">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
 }
-
-function Square() {
-    return(
-        <button type="submit">X</button>
-    )
-}
-
-export default Board;
